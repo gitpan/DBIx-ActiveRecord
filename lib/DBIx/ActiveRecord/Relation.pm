9656019d258d404b2b2811c84d4ceab5d26baedd
Toshiyuki Saito <tsaito@cpan.org>
2012-12-05 14:25:55 +0000

    Import of TSAITO/DBIx-ActiveRecord-0.01 from CPAN.
    gitpan-cpan-distribution: DBIx-ActiveRecord
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         TSAITO/DBIx-ActiveRecord-0.01.tar.gz
    gitpan-cpan-author:       TSAITO
    gitpan-cpan-maturity:     released

---
+++ lib/DBIx/ActiveRecord/Relation.pm
@@ -0,0 +1,57 @@
+package DBIx::ActiveRecord::Relation;
+use strict;
+use warnings;
+use Carp;
+
+use DBIx::ActiveRecord;
+use DBIx::ActiveRecord::Scope;
+
+use overload '@{}' => \&all;
+
+sub new {
+    my ($self, $model) = @_;
+    bless {model => $model, arel => $model->arel}, $self;
+}
+
+sub scoped {
+    my ($self) = @_;
+    my $s = __PACKAGE__->new($self->{model});
+    $s->{arel} = $self->{arel}->clone;
+    $s;
+}
+
+sub to_sql {shift->{arel}->to_sql}
+sub _binds {shift->{arel}->binds}
+
+our $AUTOLOAD;
+sub AUTOLOAD {
+    my $self = shift;
+    $AUTOLOAD =~ /([^:]+)$/;
+    my $m = $1;
+    my $s = $self->{model}->_global->{scopes}->{$m};
+    croak "method missing $AUTOLOAD" if !$s;
+    $s->($self->scoped, @_);
+}
+
+sub DESTROY{}
+
+sub all {
+    my $self = shift;
+    $self->{cache}->{all} ||= DBIx::ActiveRecord::Scope::all($self, @_);
+}
+
+sub first {
+    my $self = shift;
+    return $self->{cache}->{all}->[0] if $self->{cache}->{all};
+    return $self->{cache}->{first} if exists $self->{cache}->{first};
+    $self->{cache}->{first} = DBIx::ActiveRecord::Scope::first($self, @_);
+}
+
+sub last {
+    my $self = shift;
+    return $self->{cache}->{all}->[-1] if $self->{cache}->{all};
+    return $self->{cache}->{last} if exists $self->{cache}->{last};
+    $self->{cache}->{last} = DBIx::ActiveRecord::Scope::last($self, @_);
+}
+
+1;
